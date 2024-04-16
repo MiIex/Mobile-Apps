0589121b9a5366483fa024622833428227c36354
Felix Hoffmann <miiex.m.miiex@gmail.com>
2024-04-16 15:21:43 +0200

    fix: readded routes

--- Mobile-Applications/src/router.ts
+++ Mobile-Applications/src/router.ts
@@ -3,8 +3,9 @@ import { createMemoryHistory, createRouter, createWebHistory } from 'vue-router'
 import LogInView from './LogIn-Area/view/LogInView.vue'
 import ChatView from './chat-area/view/ChatView.vue'
 import RegistryView from './registry-area/view/registryView.vue'
-
+import SettingView from './settings-area/view/SettingView.vue'
 import GroupChatView from './groupChat-Area/view/GroupChatView.vue'
+import ProfileSettingView from './profile-settings-area/view/ProfileSettingView.vue'
 
 const routes = [
   { path: '/', redirect: '/login' },
@@ -14,7 +15,7 @@ const routes = [
   { path: '/groupchat', component: GroupChatView },
   { path: '/settings', component: SettingView},
   { path: '/profilesettings', component: ProfileSettingView}
-
+]
 export const router = createRouter({
   history: createWebHistory(),
   routes,
