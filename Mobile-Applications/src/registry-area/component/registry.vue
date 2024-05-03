bd24743e2acdd6245c0d7b0b57662c0b7f4bf29e
Felix Hoffmann <miiex.m.miiex@gmail.com>
2024-05-03 13:51:20 +0200

    add registry

--- Mobile-Applications/src/registry-area/component/registry.vue
+++ Mobile-Applications/src/registry-area/component/registry.vue
@@ -5,32 +5,38 @@
         <InputGroupAddon>
           <i class="pi pi-user"></i>
         </InputGroupAddon>
-        <InputText placeholder="Username" />
+        <InputText placeholder="Username" v-model="username"/>
       </InputGroup>
       <InputGroup>
         <InputGroupAddon>
           <i class="pi pi-user"></i>
         </InputGroupAddon>
-        <InputText placeholder="Nickname" />
+        <InputText placeholder="Nickname" v-model="nickname"/>
+      </InputGroup>
+      <InputGroup>
+        <InputGroupAddon>
+          <i class="pi pi-user"></i>
+        </InputGroupAddon>
+        <InputText placeholder="Name" v-model="fullname"/>
       </InputGroup>
       <InputGroup>
         <InputGroupAddon>
           <i class="pi pi-key"></i>
         </InputGroupAddon>
-        <Password v-model="value" placeholder="Passwort" toggleMask :feedback="false" />
+        <Password v-model="password" placeholder="Passwort" toggleMask :feedback="false" />
       </InputGroup>
       <InputGroup>
         <InputGroupAddon>
           <i class="pi pi-key"></i>
         </InputGroupAddon>
-        <Password v-model="value" placeholder="Passwort wiederholen" toggleMask :feedback="false" />
+        <Password v-model="repeatPassword" placeholder="Passwort wiederholen" toggleMask :feedback="false" />
       </InputGroup>
       <div class="flex align-items-center">
         <label for="stay-signed-in" class="ml-2"> Eingeloggt bleiben </label>
-        <Checkbox v-model="checked" inputId="stay-signed-in" name="pizza" value={{checked}} />
+        <Checkbox v-model="checked" inputId="stay-signed-in"  value={{checked}} />
       </div>
       
-        <Button label="Account erstellen"  class="registry-btn"></Button>
+        <Button label="Account erstellen" @click="registry(username, nickname, password, repeatPassword, fullname)" class="registry-btn"></Button>
      
       <RouterLink to="/login" tag="button">
         <Button label="Zurück zum Login" class="login-btn" link></Button>
@@ -42,12 +48,41 @@
 
 <script setup>
 import { ref } from 'vue'
+import axios from 'axios';
+var username = ref(null);
+var nickname = ref(null);
+var password = ref(null);
+var repeatPassword = ref(null);
+var fullname = ref(null)
 
-const value = ref(null);
 let checked = ref(false)
 </script>
 
-
+<script>
+export default {
+  methods: {
+    async registry(username, nickname, password, repeatPassword, fullname) {
+      console.log(password)
+      console.log(repeatPassword)
+      if(password === repeatPassword){
+      let result = await axios.get("https://www2.hs-esslingen.de/~melcher/map/chat/api/?request=register", {
+        params: {
+          userid: username,
+          password: password,
+          nickname: nickname,
+          fullname: fullname,
+        }
+      })
+      
+      this.$store.commit('logIn', result)
+      this.$router.push("/chat")
+    } else {
+      console.warn("passwörter stimmen nicht überein")
+    }
+    }
+  },
+}
+</script>
 
 <style lang="scss" scoped>
   .login-container{
