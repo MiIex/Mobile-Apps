f05c0f2cfffdf343726bb0977cef423af24b7d4c
Rop1ck <106846210+Rop1ck@users.noreply.github.com>
2024-05-30 20:30:21 +0200

    darkmode fix

--- Mobile-Applications/src/global/App.vue
+++ Mobile-Applications/src/global/App.vue
@@ -9,21 +9,23 @@ import { usePrimeVue } from 'primevue/config';
 import { useStore } from 'vuex';
 
 const store = useStore();
-
 const PrimeVue = usePrimeVue();
 
 const storedTextSize = localStorage.getItem('textSize');
 const textSizeClass = store.getters.textSize;
 
-const isDarkMode = localStorage.getItem('darkmode') === 'true';
-
-if (isDarkMode) {
-    PrimeVue.changeTheme('md-light-deeppurple', 'lara-dark-indigo', 'theme-link', () => {
-        console.log('Theme changed to Dark');
-    });
-} else {
-    PrimeVue.changeTheme('lara-dark-indigo', 'md-light-deeppurple', 'theme-link', () => {
-        console.log('Theme changed to Light');
-    });
-}
+// Watcher, um Änderungen des Dark Mode im Store zu überwachen
+store.watch(() => store.state.isDarkMode, (newValue) => {
+    if (newValue) {
+        // Dark Mode ist aktiv
+        PrimeVue.changeTheme('md-light-deeppurple', 'lara-dark-indigo', 'theme-link', () => {
+            console.log('Theme changed to Dark');
+        });
+    } else {
+        // Dark Mode ist deaktiviert
+        PrimeVue.changeTheme('lara-dark-indigo', 'md-light-deeppurple', 'theme-link', () => {
+            console.log('Theme changed to Light');
+        });
+    }
+});
 </script>
