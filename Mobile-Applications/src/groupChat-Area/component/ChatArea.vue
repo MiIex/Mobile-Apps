6fb2a1fcb5b2b9a1753fe741392a21d9be376d6a
Felix Hoffmann <miiex.m.miiex@gmail.com>
2024-06-04 12:11:51 +0200

    fix header

--- Mobile-Applications/src/groupChat-Area/component/ChatArea.vue
+++ Mobile-Applications/src/groupChat-Area/component/ChatArea.vue
@@ -1,18 +1,20 @@
 <template>
-
     <div class="messages-container">
-    <div class="header">
-        <RouterLink to="/chat" tag="button">
-            <Button><i class="pi pi-angle-left"></i></Button>
-        </RouterLink>
-        <h2>Groupchat</h2>
-    </div>
-        <template v-for="message in messages">
-            <MessagesTransmitter v-if="message.userhash == userhash" :text="message.text" :time="message.time">
-            </MessagesTransmitter>
-            <MessagesRecipient v-else :name="message.usernickname" :text="message.text" :time="message.time">
-            </MessagesRecipient>
-        </template>
+        <div class="header">
+            <RouterLink to="/chat" tag="button">
+                <Button class="chat"><i class="pi pi-angle-left"></i></Button>
+            </RouterLink>
+            <h2>Groupchat</h2>
+        </div>
+        <div class="chat-area">
+            <button @click="getMessages()" class="load-more">Mehr Nachrichten</button>
+            <template v-for="message in messages">
+                <MessagesTransmitter v-if="message.userhash == userhash" :text="message.text" :time="message.time">
+                </MessagesTransmitter>
+                <MessagesRecipient v-else :name="message.usernickname" :text="message.text" :time="message.time">
+                </MessagesRecipient>
+            </template>
+        </div>
     </div>
     <div class="background-layer" :style="backgroundLayerStyle"></div>
 </template>
@@ -30,15 +32,29 @@ const router = useRouter();
 
 let key = store.state.token;
 let userhash = store.state.userhash
-var messages = ref([]);
+let messages = ref([]);
+var shownmessages = 0
 
 onMounted(() => {
-    getMessages();
+    loadMessages();
     if (!store.state.token) {
         router.push("/login");
     }
-    console.log(store.state.userhash)
 });
+
+const loadMessages = async () => {
+    let result = await axios.get("https://www2.hs-esslingen.de/~melcher/map/chat/api/index.php/?request=getmessages", {
+        params: {
+            token: store.state.token
+        }
+    }).catch(function (error) {
+
+    })
+    let lastMessages = result.data.messages.slice(-5)
+    for (var message of lastMessages) {
+        messages.value.push({ userhash: message.userhash, text: message.text, usernickname: message.usernickname, time: message.time })
+    }
+}
 
 const getMessages = async () => {
     let result = await axios.get("https://www2.hs-esslingen.de/~melcher/map/chat/api/index.php/?request=getmessages", {
@@ -48,15 +64,15 @@ const getMessages = async () => {
     }).catch(function (error) {
 
     })
-    console.log(result.data.messages)
-    let lastMessages = result.data.messages.slice(-20)
-    console.warn(lastMessages)
+    
+
+    shownmessages = shownmessages - 5;
+    let lastMessages = result.data.messages.slice(shownmessages-5, shownmessages)
+    lastMessages = lastMessages.reverse()
     for (var message of lastMessages) {
         //messages.value.push({userhash: "VBB2mJqq", text: "hahaha"})
-        messages.value.push({ userhash: message.userhash, text: message.text, usernickname: message.usernickname, time: message.time })
+        messages.value.unshift({ userhash: message.userhash, text: message.text, usernickname: message.usernickname, time: message.time })
     }
-    console.log(messages.value)
-    console.log(store.state.userhash)
 }
 
 const backgroundImageUrl = ref(localStorage.getItem('backgroundImageUrl') || '');
@@ -70,7 +86,6 @@ const backgroundLayerStyle = computed(() => ({
 </script>
 
 <style scoped>
-
 .messages-container {
     max-height: 90vh;
     overflow-y: auto;
@@ -87,18 +102,34 @@ const backgroundLayerStyle = computed(() => ({
     opacity: 0.5;
 }
 
-.header{
+.header {
     display: flex;
     height: 60px;
     border: solid;
     border-width: 1px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 999;
+    width: 100%;
+    background-color: #1f2937;
 }
 
-Button{
-  height: 40px;
-  top: 10px;
- 
+.chat-area{
+    margin-top: 60px;
 }
 
+.chat {
+    height: 40px;
+    top: 10px;
 
+}
+
+.load-more {
+    height: 20px;
+    
+    margin: auto;
+    display:block;
+    margin-bottom: 10px;
+}
 </style>
